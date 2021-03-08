659104f85354bc5c0ef1abfc493fa178242cf823
Joe <joseph.lee3454@gmail.com>
2021-03-07 19:11:02 -0800

    added toggle use state

--- src/App.js
+++ src/App.js
@@ -5,17 +5,40 @@ import React, {useState} from 'react'
 const App=()=> {
   const [count, setCount] = useState(0)//state var we start at 0
 
+  const [light,setLight] = useState(false)
 
 
 
   const incrementCount = () => {
     setCount(prevCount => prevCount + 1)//should now increment from the previous value 
   }
+
+  const toggleLight = () => {
+    setLight(prevLight => !prevLight)
+  }
+
+
 /// return butt while calling onclick with theh incrementCount being called
   return (
+    <>
+
+    <h2>Coiunter</h2>
     <button onClick={incrementCount}>
       i was clicked {count}
     </button>
+
+    <h1>Toggle Light</h1>
+    <div
+    style={{
+      height: "50px",
+      width: "50px",
+      background: light ? "yellow" : "grey"
+
+
+    }}
+    onClick={toggleLight}
+    />
+    </>
   );
 }
 
