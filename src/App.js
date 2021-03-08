55543318e7c93389518a00a8b81d9e951b74ccf2
Joe <joseph.lee3454@gmail.com>
2021-03-07 19:00:28 -0800

    added simple prev state

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import React, {useState} from 'react'
 
 const App=()=> {
   const [count, setCount] = useState(0)//state var we start at 0
+
+
+
+
   const incrementCount = () => {
-    setCount(count + 1)//function to increment state 
+    setCount(prevCount => prevCount + 1)//should now increment from the previous value 
   }
 /// return butt while calling onclick with theh incrementCount being called
   return (
